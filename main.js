a3da922c308dd1abcdb41dc0a17d3cb0f3e42ac7
Gunnar <gnnrbachelor@gmail.com>
2023-10-23 09:45:59 -0600

    Debugging next step

--- main.js
+++ main.js
@@ -1,75 +1,90 @@
-   import * as data from 'ethData';
-    
-    
-    // Three.js scene setup
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(renderer.domElement);
+import { getLatestBlockAndTransactions } from './eth-data';
 
-    // Cannon.js physics setup
-    const world = new CANNON.World();
-    world.gravity.set(0, -10, 0);
-    
-    // Create a ground plane
-    const groundShape = new CANNON.Plane();
-    const groundBody = new CANNON.Body({ mass: 0 });
-    groundBody.addShape(groundShape);
-    groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2);
-    world.addBody(groundBody);
+const sphereToTX = [];
+// Three.js scene setup
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-    // Create spheres and add them to the scene
-    const sphereGeometry = new THREE.SphereGeometry(0.2, 32, 32);
-    const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+// Cannon.js physics setup
+const world = new CANNON.World();
+world.gravity.set(0, -10, 0);
 
-    function createSphere(x, y, z) {
-        const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
-        scene.add(sphereMesh);
+// Create a ground plane
+const groundShape = new CANNON.Plane();
+const groundBody = new CANNON.Body({ mass: 0 });
+groundBody.addShape(groundShape);
+groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2);
+world.addBody(groundBody);
 
-        const sphereShape = new CANNON.Sphere(0.2);
-        const sphereBody = new CANNON.Body({ mass: 1, shape: sphereShape });
-        sphereBody.position.set(x, y, z);
-        world.addBody(sphereBody);
+// Create spheres and add them to the scene
+const sphereGeometry = new THREE.SphereGeometry(0.2, 32, 32);
+const sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 
-        // Link the Three.js mesh to the Cannon.js body
-        sphereBody.threeMesh = sphereMesh;
+function createSphere(x, y, z, txh) {
 
-        // Add a callback to keep the mesh position in sync with the body
-        sphereBody.addEventListener('collide', function(event) {
-            console.log('Sphere collided with', event.body);
-        });
-    }
+    const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+    scene.add(sphereMesh);
+    sphereToTX.push({ [sphereMesh.id]: txh });
+    console.log(sphereToTX);
 
-    // Create falling spheres
+
+
+    const sphereShape = new CANNON.Sphere(0.2);
+
+    const sphereBody = new CANNON.Body({ mass: 1, shape: sphereShape });
+
+    sphereBody.position.set(x, y, z);
+    world.addBody(sphereBody);
+
+    // Link the Three.js mesh to the Cannon.js body
+    sphereBody.threeMesh = sphereMesh;
+
+
+    // Add a callback to keep the mesh position in sync with the body
+    sphereBody.addEventListener('collide', function (event) {
+        console.log('Sphere collided with', event.body);
+    });
+}
+
+const providerUrl = 'https://eth-mainnet.blastapi.io/7ba8e1ac-14a0-4e49-a96e-adb82420a114';
+
+const block = await getLatestBlockAndTransactions(providerUrl);
+
+// Create falling spheres
+if (2===2) {
     for (let i = 0; i < 10; i++) {
         const x = Math.random() * 8 - 4; // Random x position within the scene
         const y = 5; // Initial height
         const z = Math.random() * 8 - 4; // Random z position within the scene
-
-        createSphere(x, y, z);
+        const txh = block.transactionHashes[i];
+        createSphere(x, y, z, txh);
     }
 
-    // Animation function
-    function animate() {
-        requestAnimationFrame(animate);
+}
 
-        // Step the Cannon.js world
-        world.step(1 / 60);
+// Animation function
+function animate() {
+    requestAnimationFrame(animate);
 
-        // Update sphere positions
-        world.bodies.forEach(function (body) {
-            if (body.threeMesh) {
-                body.threeMesh.position.copy(body.position);
-                body.threeMesh.quaternion.copy(body.quaternion);
-            }
-        });
+    // Step the Cannon.js world
+    world.step(1 / 60);
 
-        renderer.render(scene, camera);
-    }
+    // Update sphere positions
+    world.bodies.forEach(function (body) {
+        if (body.threeMesh) {
+            body.threeMesh.position.copy(body.position);
+            body.threeMesh.quaternion.copy(body.quaternion);
+        }
+    });
 
-    // Set camera position
-    camera.position.z = 5;
+    renderer.render(scene, camera);
+}
 
-    // Start the animation
-    animate();
+// Set camera position
+camera.position.z = 5;
+
+// Start the animation
+animate();
